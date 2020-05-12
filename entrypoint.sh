42757c1862313d97a56b821c72c8f5b36943e9a0
Adam Christie <fractos@gmail.com>
2020-05-12 23:14:49 +0100

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ echo "Downloading kubectl ${KUBECTL_VERSION}"
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl /usr/local/bin
 
-sh -c "kubectl --version --short""
+sh -c "kubectl --version --short"
