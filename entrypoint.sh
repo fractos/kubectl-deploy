7ab05e882f77d4db4a5e0068a40e6be0791a73fd
Adam Christie <fractos@gmail.com>
2020-05-12 23:52:56 +0100

    logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,24 @@ echo "Downloading kubectl ${KUBECTL_VERSION}"
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl 2> /dev/null && chmod +x kubectl && mv kubectl /usr/local/bin
 
-sh -c "kubectl version --short"
-sh -c "kubectl get pods"
+echo "Applying deployment ${DEPLOYMENT_FILE}..."
+sh -c "kubectl apply -f ${DEPLOYMENT_FILE}"
+
+if [ $? -eq 0 ]
+then
+  echo "Verifying deployment status..."
+  sh -c "kubectl rollout status deployment/${DEPLOYMENT_NAME} | tee rollout.txt"
+  if [ $? -eq 0]
+  then
+    echo "Deployment rollout verified"
+    exit 0
+  else
+    echo "Deployment rollout failed!"
+    cat rollout.txt
+    exit 1
+  fi
+else
+  echo "Deployment could not be applied!"
+  exit 1
+fi
+
